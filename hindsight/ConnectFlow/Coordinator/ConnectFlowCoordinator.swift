ab94c4a09c99db489db671e837daa6c64597cfae
Yue X Liu <yue.x.liu@chase.com>
2018-12-26 16:48:06 -0500

    Working on RX porting

--- hindsight/ConnectFlow/Coordinator/ConnectFlowCoordinator.swift
+++ hindsight/ConnectFlow/Coordinator/ConnectFlowCoordinator.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import Swinject
 import FBSDKLoginKit
+import FacebookLogin
 
 protocol ConnectFlowCoordinatorProtocol {
     func presentLogInAsRoot(nc: UINavigationController)
@@ -27,19 +28,28 @@ struct ConnectFlowCoordinator: ConnectFlowCoordinatorProtocol, PresenterProvidin
         self.presenter = presenter
         self.container = container
         self.navigationController = nc
+        //  TODO: @manish why static func here?
         DependencyConfigurator.registerConnectFlowDependencies(container: container)
-
     }
 
+    //  TODO: @manish remove nc?
     func presentLogInAsRoot(nc: UINavigationController) {
         let vm = LoginViewModel(facebookConnectClosure: {
-            Connector(container: self.container, vc: self.navigationController).facebookConnect()
-		})
+            let bag = DisposeBag()
+            let subscription = Connector(container: self.container, vc: self.navigationController)
+                .facebookConnect()
+                .subscribe { event in
+                    print("FB event: \(event)")
+                }
+            subscription.disposed(by: bag)
+        })
         let vc = LoginViewController(viewModel: vm)
         navigationController.isNavigationBarHidden = true
         presenter.makeRoot(vc: vc, nc: navigationController)
     }
 }
+
+import RxSwift
 
 struct Connector {
 
@@ -52,33 +62,45 @@ struct Connector {
         self.container = container
         self.viewController = vc
         self.client = container.resolveUnwrapped(ConnectApiClientProtocol.self)
+        loginManager.logOut()
     }
 
-    func facebookConnect() {
-
-        if let token = FBSDKAccessToken.current() {
-            print("FB token exists", token.tokenString)
-            self.client.connect(token: token.tokenString)
-            return
-        }
-
-        loginManager.logIn(withReadPermissions: ["public_profile", "user_friends", "email"],
-            from: viewController) { loginResult, error in
-
-            if let error = error {
-                print("FB failed", error)
-                return
+    func facebookConnect() -> Observable<Bool> {
+        return Observable<Bool>.create { observer -> Disposable in
+            if let token = FBSDKAccessToken.current() {
+                print("FB token exists", token.tokenString)
+                self.client.connect(token: token.tokenString)
+                observer.onNext(false)
+                observer.onCompleted()
+                return Disposables.create()
             }
-            guard let result = loginResult else {
-                print("FB invalid result")
-                return
+
+            self.loginManager.logIn(withReadPermissions: ["public_profile", "user_friends", "email"], from: self.viewController) { loginResult, error in
+
+                if let error = error {
+                    observer.onNext(false)
+                    observer.onCompleted()
+                    print("FB failed", error)
+                    return
+                }
+                guard let result = loginResult else {
+                    observer.onNext(false)
+                    observer.onCompleted()
+                    print("FB invalid result")
+                    return
+                }
+                guard let token = result.token else {
+                    observer.onNext(false)
+                    observer.onCompleted()
+                    print("FB invalid token")
+                    return
+                }
+                print("FB token obtained", token.tokenString)
+                self.client.connect(token: token.tokenString)
+                observer.onNext(true)
+                observer.onCompleted()
             }
-            guard let token = result.token else {
-                print("FB invalid token")
-                return
-            }
-            print("FB token obtained", token.tokenString)
-            self.client.connect(token: token.tokenString)
+            return Disposables.create()
         }
     }
 }
